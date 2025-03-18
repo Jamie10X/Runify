4140345483569096e154a86fda9515d1da2fe379
jamie <jamshidboynazarov0@gmail.com>
2025-03-18 11:51:06 +0900

    Add utilities to hande errors and one time event state changes.

--- core/domain/src/main/java/com/jamie/core/domain/util/DataError.kt
+++ core/domain/src/main/java/com/jamie/core/domain/util/DataError.kt
@@ -1,11 +1,13 @@
 package com.jamie.core.domain.util
 
 sealed interface DataError: Error {
+
     enum class Network: DataError {
         REQUEST_TIMEOUT,
         UNAUTHORIZED,
         CONFLICT,
         TOO_MANY_REQUESTS,
+        NO_INTERNET,
         PAYLOAD_TOO_LARGE,
         SERVER_ERROR,
         SERIALIZATION_ERROR,
